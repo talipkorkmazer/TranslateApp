34203deefb7a88ecac77362848993aef47335651
talip <talip@mnm.com.tr>
2019-10-03 00:48:17 +0300

    go back button added

--- resources/views/translate.blade.php
+++ resources/views/translate.blade.php
@@ -2,8 +2,10 @@
 
 @section('content')
     @if($status == true)
-        <h2 class="is-size-3">{!! $text !!}</h2>
+        <h2 class="is-size-3 has-text-centered">{!! $text !!}</h2>
     @else
         <h2 class="is-size-3">{{$message}}</h2>
     @endif
+    <a href="/">Go Back</a>
 @endsection
+
